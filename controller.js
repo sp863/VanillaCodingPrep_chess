263891685a1b4ef34be9a29d458b8ad859e640fe
sp863 <sp863429@gmail.com>
2022-01-02 23:45:27 +0900

    chess: index css

--- controller.js
+++ controller.js
@@ -94,37 +94,3 @@ console.log(model.gameData.playerWhitePieceList);
 console.log(model.gameData.playerBlackPieceList);
 console.log(model.gameData.totalPieceList);
 console.log(gamePlayStatus.currentKing);
-
-// KING ON CHECK TESTING
-// console.log(model.isBlackKingOnCheck(View.tileEmpty, View.getElementOnTile));
-// console.log(model.isWhiteKingOnCheck(View.tileEmpty, View.getElementOnTile));
-
-// PROMOTION TESTING
-// model.pawnPromotion("wp1", "queen");
-// model.updateTotalList();
-// View.updateBoard(model.gameData);
-
-// MOVE TESTING
-// //King
-// console.log("-------------- King --------------");
-// model.unitMove("wk0", [2, 7], View.tileEmpty, View.getElementOnTile);
-// //Queen
-// console.log("-------------- Queen --------------");
-// model.unitMove("wq0", [2, 7], View.tileEmpty, View.getElementOnTile);
-// //Bishop
-// console.log("-------------- Bishop --------------");
-// model.unitMove("wb1", [2, 7], View.tileEmpty, View.getElementOnTile);
-// //Knight
-// console.log("-------------- Knight --------------");
-// model.unitMove("wn1", [2, 7], View.tileEmpty, View.getElementOnTile);
-// //Rook
-// console.log("-------------- Rook --------------");
-// model.unitMove("wr1", [2, 7], View.tileEmpty, View.getElementOnTile);
-//Pawn
-// console.log("-------------- White Pawn --------------");
-// model.unitMove("wp5", [5, 4], View.tileEmpty, View.getElementOnTile); //true -> one tile
-// model.unitMove("wp5", [4, 4], View.tileEmpty, View.getElementOnTile); //false -> two tiles
-// model.unitMove("wp5", [5, 5], View.tileEmpty, View.getElementOnTile); //false
-// model.unitMove("wp5", [5, 3], View.tileEmpty, View.getElementOnTile); //false
-// console.log("-------------- Black Pawn --------------");
-// model.unitMove("bp5", [2, 4], View.tileEmpty, View.getElementOnTile); //false
